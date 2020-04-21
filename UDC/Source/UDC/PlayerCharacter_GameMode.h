757ff68dfb328c3880fca2b481aae7edf610915d
MartSchilt <schilthuismart@gmail.com>
2020-04-21 12:09:50 +0200

    added character + controls

---
+++ UDC/Source/UDC/PlayerCharacter_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PlayerCharacter_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDC_API APlayerCharacter_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
