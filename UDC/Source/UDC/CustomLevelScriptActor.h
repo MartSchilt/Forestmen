7b62af9db2816262f639dcd04173726bf0652767
MartSchilt <schilthuismart@gmail.com>
2020-04-22 11:51:06 +0200

    added custom levelscriptactor

---
+++ UDC/Source/UDC/CustomLevelScriptActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "CustomLevelScriptActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDC_API ACustomLevelScriptActor : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
