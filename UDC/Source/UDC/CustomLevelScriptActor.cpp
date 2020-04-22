d453e5187962363aaa82b113c51cd01566e9b826
MartSchilt <schilthuismart@gmail.com>
2020-04-22 16:25:52 +0200

    working random generation

--- UDC/Source/UDC/CustomLevelScriptActor.cpp
+++ UDC/Source/UDC/CustomLevelScriptActor.cpp
@@ -3,3 +3,38 @@
 
 #include "CustomLevelScriptActor.h"
 
+ACustomLevelScriptActor::ACustomLevelScriptActor() {
+	GridSize = 9;
+	TileSize = 1000,0;
+	StreamingLevelNames.Add(FName(TEXT("Procedural01")));
+	StreamingLevelNames.Add(FName(TEXT("Procedural02")));
+	StreamingLevelNames.Add(FName(TEXT("Procedural03")));
+}
+
+float ACustomLevelScriptActor::GetLocation(int index)
+{
+	// Multiple index times the tilesize and that minus the mapsize will be the x or y location, depending on the given index
+	float m = index * TileSize;
+	return (m - GetMapSize());
+}
+
+float ACustomLevelScriptActor::GetMapSize()
+{
+	return (GridSize * TileSize) / 2.0f;
+}
+
+FName ACustomLevelScriptActor::GetStreamingLevelName()
+{
+	FName* data = StreamingLevelNames.GetData();
+
+	int r = FMath::RandRange(0, StreamingLevelNames.Num() - 1);
+
+	return data[r];
+}
+
+float ACustomLevelScriptActor::GetRandomRotation()
+{
+	int r = FMath::RandRange(0, 4);
+
+	return r * 90;
+}
--- UDC/Source/UDC/CustomLevelScriptActor.h
+++ UDC/Source/UDC/CustomLevelScriptActor.h
@@ -13,5 +13,29 @@ UCLASS()
 class UDC_API ACustomLevelScriptActor : public ALevelScriptActor
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variables")
+		int GridSize;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variables")
+		float TileSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variables")
+		TArray<FName> StreamingLevelNames;
+
+	ACustomLevelScriptActor();
+
+	UFUNCTION(BlueprintCallable, Category = LevelBlueprint)
+		float GetLocation(int i);
+
+	UFUNCTION(BlueprintCallable, Category = LevelBlueprint)
+		float GetMapSize();
+
+	UFUNCTION(BlueprintCallable, Category = LevelBlueprint)
+		FName GetStreamingLevelName();
+
+	UFUNCTION(BlueprintCallable, Category = LevelBlueprint)
+		float GetRandomRotation();
+
 };
