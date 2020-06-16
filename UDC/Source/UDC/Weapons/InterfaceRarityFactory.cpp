3b895bf5d10da8095590a222a08396d9e04edc56
MartSchilt <schilthuismart@gmail.com>
2020-06-16 16:28:07 +0200

    removed interface

--- UDC/Source/UDC/Weapons/InterfaceRarityFactory.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "InterfaceRarityFactory.h"
-
-// Add default functionality here for any IInterfaceRarityFactory functions that are not pure virtual.
